9af696256dba26bb88c227261a9cc3835e2ccd43
Alexis Le-Quoc <alq@datadoghq.com>
2015-11-17 14:54:44 -0500

    Update package list and add some customization

--- init.el
+++ init.el
@@ -10,3 +10,18 @@
     )
   )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-use-system-font t)
+ '(magit-push-arguments nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 109 :width normal)))))
