e4059dd2eeef61626e107072ce39b5771d0df125
Alexis Le-Quoc <alq@datadoghq.com>
2019-02-06 18:12:45 +0000

    Marmelade is dead

--- packages.el
+++ packages.el
@@ -2,7 +2,6 @@
 ; list the repositories containing them
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (defun ensure-package-installed (&rest packages)
@@ -25,4 +24,4 @@ Return a list of installed packages or nil for every skipped package."
 (package-initialize)
 
 ;; list of dependencies here
-(mapcar 'ensure-package-installed '(2048-game ack csv-mode gist go-autocomplete auto-complete go-direx direx go-eldoc go-errcheck go-mode js2-mode magit-filenotify magit-find-file magit-gh-pulls gh logito magit markdown-mode nlinum pcache popup s))
+(mapcar 'ensure-package-installed '(2048-game ack csv-mode gist go-autocomplete auto-complete go-direx direx go-eldoc go-errcheck go-mode js2-mode gh logito markdown-mode nlinum pcache popup s))
